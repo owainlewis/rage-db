4eca3a44f01f95fba55e91501724788768768b12
Owain Lewis <owain@owainlewis.com>
2014-08-16 21:00:09 +0100

    updates project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject rage-db "0.1.0-SNAPSHOT"
   :description "Lightweight disk based json database"
-  :url ""
+  :url "http://owainlewis.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
